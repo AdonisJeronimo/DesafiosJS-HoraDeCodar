c0394336b4dca9ff6d809e6059cf1b414b792ff3
AdonisJeronimo <adonisfjeronimo@gmail.com>
2023-07-04 15:48:04 -0300

    Desafio 12

---
+++ Desafio12/js/script.js
@@ -0,0 +1,25 @@
+const M  = 50;
+
+function isPrime(num){
+    if(num <= 1){
+        return false;
+    }
+
+    for(let i = 2; i * i <= num; i++){
+        if(num % i === 0){
+            return false;
+        }
+    }
+
+    return true;
+}
+
+for(let i = 2; i <= M; i++){
+    if(isPrime(i)){
+        console.log(i);
+    }
+}
+
+
+
+
